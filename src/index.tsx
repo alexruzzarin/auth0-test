a93f07c0596ffce78858d5cb9dd4796bf41a9b4a
Alex <1187547+alexruzzarin@users.noreply.github.com>
2020-09-13 04:36:22 -0400

    diff ids

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,16 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const clientId =
+  window.location.host === 'another-auth-test.ruzzar.in'
+    ? '3U8ryvh5nB7CbPLKgafr6JpVFcaJOGSA'
+    : 'CNA7l9PJhm7s5eIv6BdVdUxzUQpfRol8';
+
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
       domain="ruzzarin.auth0.com"
-      clientId="CNA7l9PJhm7s5eIv6BdVdUxzUQpfRol8"
+      clientId={clientId}
       redirectUri={window.location.origin}
     >
       <App />
