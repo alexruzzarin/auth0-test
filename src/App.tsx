2fc9d749db01bae78f44ba20793cbf86b2069818
Alex <1187547+alexruzzarin@users.noreply.github.com>
2020-09-15 19:18:02 -0400

    auto login redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-import Profile from './Profile';
-import LoginButton from './LoginButton';
-import LogoutButton from './LogoutButton';
-import { useAuth0 } from '@auth0/auth0-react';
+import Profile from "./Profile";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
-  const { isAuthenticated } = useAuth0();
+  const { isLoading, isAuthenticated, loginWithRedirect } = useAuth0();
+
+  useEffect(() => {
+    if (!isLoading && !isAuthenticated) {
+      console.log("Not Authenticated");
+      loginWithRedirect();
+    }
+  }, [isLoading, isAuthenticated, loginWithRedirect]);
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
   return (
     <>
       <header>
-        <h1>Auth 0 Test</h1>
+        <h1>{window.location.host}</h1>
         <nav>{isAuthenticated ? <LogoutButton /> : <LoginButton />}</nav>
       </header>
       <main>
--- src/Profile.tsx
+++ src/Profile.tsx
@@ -5,7 +5,7 @@ const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
-    return <div>Loading ...</div>;
+    return <div>Loading Profile Data...</div>;
   }
 
   return isAuthenticated ? (
